84bada99432902ed10723ef8b80e06c5ed0af340
Esteban Pedraza <estemanps99@gmail.com>
2021-05-03 18:51:24 -0500

    Implementados todos los tests para sumar, sustraer, multiplicar, dividir, atrás. mód, signo, coma y 10x.

--- CalculatorGUI/test/businessLogic/CalculadoraIT.java
+++ CalculatorGUI/test/businessLogic/CalculadoraIT.java
@@ -40,15 +40,6 @@ public class CalculadoraIT {
     /**
      * Test of add method, of class Calculadora.
      */
-    /*@Test
-    public void testAdd() {
-        System.out.println("add");
-        float a = 3F;
-        float b = 5F;
-        float expResult = 8F;
-        float result = Calculadora.add(a, b);
-        assertEquals(expResult, result, 0.0);
-    }*/
 
     @Test
     public void twoAndThreeIsFive(){
@@ -93,16 +84,6 @@ public class CalculadoraIT {
     /**
      * Test of substract method, of class Calculadora.
      */
-    /*@Test
-    public void testSubstract() {
-        System.out.println("substract");
-        float a = 10F;
-        float b = 5F;
-        float expResult = 5F;
-        float result = Calculadora.substract(a, b);
-        assertEquals(expResult, result, 0.0);
-    }*/
-
     @Test
     public void threeMinusTwoIsOne() {
         System.out.println("threeMinusTwoIsOne");
@@ -146,29 +127,225 @@ public class CalculadoraIT {
     /**
      * Test of multiply method, of class Calculadora.
      */
+
     @Test
-    public void testMultiply() {
-        System.out.println("multiply");
-        float a = 7F;
-        float b = 2F;
-        float expResult = 14F;
+    public void threeXThreeIsNine() {
+        System.out.println("threeXThreeIsNine");
+        float a = 3F;
+        float b = 3F;
+        float expResult = 9F;
         float result = Calculadora.multiply(a, b);
         assertEquals(expResult, result, 0.0);
-        
+    }
+
+    @Test
+    public void threeXZeroIsZero() {
+        System.out.println("threeXZeroIsZero");
+        float a = 3F;
+        float b = 0F;
+        float expResult = 0F;
+        float result = Calculadora.multiply(a, b);
+        assertEquals(expResult, result, 0.0);
+    }
+
+    @Test
+    public void threeXMinusThreeIsMinusNine() {
+        System.out.println("threeXMinusThreeIsMinusNine");
+        float a = 3F;
+        float b = -3F;
+        float expResult = -9F;
+        float result = Calculadora.multiply(a, b);
+        assertEquals(expResult, result, 0.0);
     }
 
     /**
      * Test of div method, of class Calculadora.
      */
+
     @Test
-    public void testDiv() {
-        System.out.println("div");
-        float a = 20F;
-        float b = 5F;
+    public void eigthBetweenTwoIsFour() {
+        System.out.println("eigthBetweenTwoIsFour");
+        float a = 8F;
+        float b = 2F;
         float expResult = 4F;
         float result = Calculadora.div(a, b);
         assertEquals(expResult, result, 0.0);
-        
     }
-    
+
+    @Test
+    public void FiveBetweenZeroIsInfinity() {
+        System.out.println("FiveBetweenZeroIsInfinity");
+        float a = 5F;
+        float b = 0F;
+        float result = Calculadora.div(a, b);
+        assertTrue(Float.isInfinite(result));
+    }
+
+    @Test
+    public void TwentyBetweenTwoIsTen() {
+        System.out.println("TwentyBetweenTwoIsTen");
+        float a = 20F;
+        float b = 2F;
+        float expResult = 10F;
+        float result = Calculadora.div(a, b);
+        assertEquals(expResult, result, 0.0);
+    }
+
+    @Test
+    public void NineBetweenFourIsTwoPointTwentyFive() {
+        System.out.println("NineBetweenFourIsTwoPointTwentyFive");
+        float a = 9F;
+        float b = 4F;
+        float expResult = 2.25F;
+        float result = Calculadora.div(a, b);
+        assertEquals(expResult, result, 0.0);
+    }
+
+    /**
+     * Test of back method, of class Calculadora.
+     */
+
+    @Test
+    public void NinetyNineBackIsNine() {
+        System.out.println("NinetyNineBackIsNine");
+        String a = "99";
+        String expResult = "9";
+        String result = Calculadora.back(a);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void SevenPointOneBackIsSevenPoint() {
+        System.out.println("SevenPointOneBackIsSevenPoint");
+        String a = "7.1";
+        String expResult = "7.";
+        String result = Calculadora.back(a);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of mod method, of class Calculadora.
+     */
+
+    @Test
+    public void FourModTwoIsZero() {
+        System.out.println("FourModTwoIsZero");
+        float a = 4F;
+        float b = 2F;
+        float expResult = 0F;
+        float result = Calculadora.mod(a, b);
+        assertEquals(expResult, result, 0.0);
+    }
+
+    @Test
+    public void TenModThreeIsOne() {
+        System.out.println("TenModThreeIsOne");
+        float a = 10F;
+        float b = 3F;
+        float expResult = 1F;
+        float result = Calculadora.mod(a, b);
+        assertEquals(expResult, result, 0.0);
+    }
+
+    @Test
+    public void FiveModTwoIsOne() {
+        System.out.println("FiveModTwoIsOne");
+        float a = 5F;
+        float b = 2F;
+        float expResult = 1F;
+        float result = Calculadora.mod(a, b);
+        assertEquals(expResult, result, 0.0);
+    }
+
+    /**
+     * Test of signo method, of class Calculadora.
+     */
+    @Test
+    public void SixtySignIsMinusSixty() {
+        System.out.println("SixtySignIsMinusSixty");
+        float a = 60F;
+        float expResult = -60F;
+        float result = Calculadora.signo(a);
+        assertEquals(expResult, result, 0.0);
+    }
+
+    @Test
+    public void MinusFourtyFourSignIsFourtyFour() {
+        System.out.println("MinusFourtyFourSignIsFourtyFour");
+        float a = -44F;
+        float expResult = 44F;
+        float result = Calculadora.signo(a);
+        assertEquals(expResult, result, 0.0);
+    }
+
+    @Test
+    public void ThreeSignIsMinusThree() {
+        System.out.println("ThreeSignIsMinusThree");
+        float a = 3F;
+        float expResult = -3F;
+        float result = Calculadora.signo(a);
+        assertEquals(expResult, result, 0.0);
+    }
+
+    /**
+     * Test of coma method, of class Calculadora.
+     */
+    @Test
+    public void NineColonIsNinePoint() {
+        System.out.println("NineColonIsNinePoint");
+        String a = "9";
+        String expResult = "9.";
+        String result = Calculadora.coma(a);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void ZeroPointOneColonIsZeroPointOne() {
+        System.out.println("ZeroPointOneColonIsZeroPointOne");
+        String a = "0.1";
+        String expResult = "0.1";
+        String result = Calculadora.coma(a);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void ZeroColonIsZeroPoint() {
+        System.out.println("ZeroColonIsZeroPoint");
+        String a = "0";
+        String expResult = "0.";
+        String result = Calculadora.coma(a);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of 10x method, of class Calculadora.
+     */
+
+    @Test
+    public void TenToThePowerOfFourIsTenThousand() {
+        System.out.println("TenToThePowerOfFourIsTenThousand");
+        float a = 4F;
+        float expResult = 10000F;
+        float result = Calculadora.tenPow(a);
+        assertEquals(expResult, result, 0.0);
+    }
+
+    @Test
+    public void TenToThePowerOfMinusTwoIs() {
+        System.out.println("TenToThePowerOfMinusTwoIs");
+        float a = -2F;
+        float expResult = 0.01F;
+        float result = Calculadora.tenPow(a);
+        assertEquals(expResult, result, 0.0);
+    }
+
+    @Test
+    public void TenToThePowerOfSixIsOneMillion() {
+        System.out.println("TenToThePowerOfSixIsOneMillion");
+        float a = 6F;
+        float expResult = 1000000F;
+        float result = Calculadora.tenPow(a);
+        assertEquals(expResult, result, 0.0);
+    }
+
 }
